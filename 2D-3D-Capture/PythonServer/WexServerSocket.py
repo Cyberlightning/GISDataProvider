d92adc59568703500f82d5d856a380238d0ced14
dtbwije <tharanga.wijethilake@cyberlightning.com>
2013-09-13 14:51:31 +0300

    Python server completed with threading support

--- 2D-3D-Capture/PythonServer/WexServerSocket.py
+++ 2D-3D-Capture/PythonServer/WexServerSocket.py
@@ -1,10 +1,13 @@
-import socket ,re  
+import re  
 import struct   
 from base64 import b64encode
 from hashlib import sha1
-import thread
+import gevent
+from gevent import socket
 from PIL import Image
-import io
+import json
+import time ,sys 
+
 
 
 '''
@@ -20,49 +23,40 @@ response_header =('HTTP/1.1 101 Switching Protocols',
 
 magic_string = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
-class BackEndServerSocket :
-    ANY = ''
-    PORT = 17000
-    LOCALHOST = "127.0.0.1"
-    BUFFER = ""
+ANY = ''
+PORT = 17000
+LOCALHOST = "127.0.0.1"
+
+
+def encodeMessage( message):
+    message = b64encode(message)
+    b1 =0x80 | 0x1 & 0x0f
+    payload_len = len(message)
+    header = struct.pack('>BB', b1, payload_len)
+    message= header +message
+    return message
     
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
-            
-    def setup(self):
-        try :
-            self.sock.bind((self.ANY, self.PORT))
-            self.sock.listen(10)
-        except socket.error as message:
-            print 'Bind failed. Error Code : '  + str(message[0]) + ' Message ' + message[1] 
-    
-    def connectionHandler(self, conn, addr):
-        f = {'fin'          : 0,
-             'opcode'       : 0,
-             'masked'       : False,
-             'hlen'         : 2,
-             'length'       : 0,
-             'payload'      : None,
-             'left'         : 0,
-             'close_code'   : 1000,
-             'close_reason' : ''}
-        
-        print 'Connected with ' + addr[0] + ':' + str(addr[1])
-        while True:
-            #It is easier to define this rather than using numbers. Reason is the value should be updated on the value of the second byte(unmaksed)
-            print 'receiving data'  
+def connectionHandler( conn, addr):
+    filename= ""
+    BUFFER = ""             
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    while True:
+        try:
+                    #It is easier to define this rather than using numbers. Reason is the value should be updated on the value of the second byte(unmaksed)
+            print 'receiving data :' +filename 
             data = conn.recv(4096)
-            self.BUFFER += data   
-            buf = self.BUFFER           
-            print 'Received data length'
-            print len(data)
-             
-            #working
+            BUFFER += data   
+            buf = BUFFER           
+                #print 'Received data length'
+                #print len(data)
+                     
+                #working
             if len(data) == 0:
-                break 
+                return
             key = re.search('Sec-WebSocket-Key:\s+(.*?)[\n\r]+', data)
             if key is not None:
                 print data
+                    #print len(key)
                 key = key.groups()[0].strip()
                 if key is not None:
                     response_key = b64encode(sha1(key + magic_string).digest())
@@ -72,27 +66,29 @@ class BackEndServerSocket :
                     print len(response)
                     print '#####################################'
                     conn.send(response)
-                key = ""
-                self.BUFFER = ""                
+                    message = "Hello Client\n..Server is listening..!"                                              
+                    conn.send(encodeMessage(message))
+                    key = ""
+                    BUFFER = ""                
             else :
                 payload_start = 2                
-                
-                # try to pull first two bytes
+                        
+                        # try to pull first two bytes
                 b1 = ord(buf[0])
                 fin = b1 & 0x80      # 1st bit
-                # next 3 bits reserved
+                        # next 3 bits reserved
                 opcode = b1 & 0x0f   # low 4 bits
                 b2 = ord(buf[1])
                 mask = b2 & 0x80    # high bit of the second byte
                 length = b2 & 0x7f   # low 7 bits of the second byte
-                #print "<!---------------------------------------!>"
-                #print "OPCODE"
-                #print opcode
-                #print "Length"                
-                #print length
-                #print "FIN"
-                #print fin
-                # check that enough bytes remain
+                        #print "<!---------------------------------------!>"
+                        #print "OPCODE"
+                        #print opcode
+                        #print "Length"                
+                        #print length
+                        #print "FIN"
+                        #print fin
+                        # check that enough bytes remain
                 if len(buf) < payload_start + 4:
                     return
                 elif length == 126:
@@ -101,85 +97,87 @@ class BackEndServerSocket :
                 elif length == 127:
                     length, = struct.unpack_from('>xxQ', buf)
                     payload_start += 8
-                #print "LENGTH OF THE PAYLOAD"
-                #print length
-                #print "Un-masking"                                
+                print "LENGTH OF THE PAYLOAD"
+                print length
+                        #print "Un-masking"                                
                 if mask:
                     mask_bytes = [ord(b) for b in buf[payload_start:payload_start + 4]]
-                    #print "MASK"
-                    #print mask_bytes
+                            #print "MASK"
+                            #print mask_bytes
                     payload_start += 4
                 full_len = payload_start + length
-                #print "FULL LENGTH"
-                #print full_len
-                #print len(buf)
+                        #print "FULL LENGTH"
+                        #print full_len
+                        #print len(buf)
                 if len(buf) < full_len:
                     print "Not a complete frame"
                 else:
                     print 'There is a complete frame now' 
                     print "check the buffer"
-                    print len(self.BUFFER)
+                    print len(BUFFER)
                     #print self.BUFFER
-                    #print "<!---------------------------------------!>"
-                        # is there a complete frame in the buffer?
-                                      
-                        # remove leading bytes, decode if necessary, dispatch
-                        
-                    payload = buf[payload_start:full_len]
-                    # use xor and mask bytes to unmask data
-                    if mask:  
-                        print mask_bytes                        
-                        print "<!======================================!>"
-                        unmasked = [mask_bytes[i % 4] ^ ord(b) for b, i in zip(payload, range(len(payload)))]
-                        print unmasked[1:250]
-                        print "<!======================================!>"
-                        payload = "".join([chr(c) for c in unmasked])
-                        #print binascii.hexlify(payload)
-                        print "<!======================================!>"
+                            #print "<!---------------------------------------!>"
+                                # is there a complete frame in the buffer?
+                                              
+                                # remove leading bytes, decode if necessary, dispatch
                                 
-                    if opcode == 1:
-                        s = payload.decode("UTF8")
-                        print s
-                        self.BUFFER=""
-                        buf= ""
-                        conn.close()
-                        break 
-                    if opcode == 2:                        
-                        print 'Handling binary data'
-                        #print payload
-                        stream = io.BytesIO(payload);
-#                         img = Image.open(stream)
-#                         img.save("a_test.png")
-                        size = 640 ,427
-                        img = Image.frombuffer('RGBA', size, payload)
-                        img.save('image.png')
-                        #print payload.decode("UTF8")
-                        #print binascii.b2a_qp(payload[0:20])
-                        #print payload[0:20]                                                        
-                        print '##### frame processed###'        
-                        print 'Closing the connection'
-                        conn.close()
-                        self.BUFFER=""
-                        buf= ""
-                        break
-        
-    def frameHanlder(self)  :
-        print 'this is not implemented yet'
-        
-    def runServer(self):
-        while True:
-            #wait to accept a connection - blocking call
-            conn, addr = self.sock.accept()
-            self.connectionHandler(conn, addr)    
-            
-    def closeConnection(self):
-        self.sock.close()
-              
-def main():
-    ss = BackEndServerSocket();
-    ss.setup();
-    ss.runServer() 
-              
+                    payload = buf[payload_start:full_len]
+                            # use xor and mask bytes to unmask data
+                    if mask:  
+                                #print mask_bytes                        
+                                #print "<!======================================!>"
+                        unmasked = [mask_bytes[i % 4] ^ ord(b) for b, i in zip(payload, range(len(payload)))]
+                                #unmasked = reversed(unmasked)
+                               #print "<!======================================!>"
+                        payload = "".join([chr(c) for c in unmasked])
+                                #print binascii.hexlify(payload)
+                                #print "<!======================================!>"
+                                        
+                        if opcode == 1:
+                            s = payload.decode("UTF8")
+                            jsondata = json.loads(s)
+                            filename= jsondata["type"]+"_"+jsondata["time"]+"."+jsondata["ext"]
+                            print(filename)
+                                #time.sleep(1)
+                            message= "FILENAME"                            
+                            sent = conn.send(encodeMessage(message))                           
+                            print "BYTES SENT"
+                            print sent
+                            BUFFER=""
+                            buf= ""
+                                #conn.close()
+                                #break 
+                        elif opcode == 2:                        
+                            print 'Handling binary data'
+                            size = 640 ,427
+                            img = Image.frombuffer('RGBA', size, payload,'raw','RGBA',0,1)
+                                #img.save("image1.png")
+                            img.save(filename)                                                      
+                            print '##### frame processed###'        
+                            print 'Closing the connection'
+                            conn.close()
+                            BUFFER=""
+                            buf= ""
+                            filename = ""
+                            return
+        except KeyboardInterrupt:
+            conn.close();
+            print "INTERRUPTED!"
+            sys.exit(0)   
     
-if __name__ == '__main__':
-    main()
+def frameHanlder()  :
+    print 'this is not implemented yet'
+   
+def closeConnection(self):
+    sock.close()
+             
+try :
+    sock= socket.socket()
+    sock.bind((ANY, PORT))
+    sock.listen(500)
+    while True:
+    #wait to accept a connection - blocking call
+        conn, addr = sock.accept()
+        gevent.spawn(connectionHandler, conn, addr)
+except socket.error as message:
+    print 'Bind failed. Error Code : '  + str(message[0]) + ' Message ' + message[1]              
