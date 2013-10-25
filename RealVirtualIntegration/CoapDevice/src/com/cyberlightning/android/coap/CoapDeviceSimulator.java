38acf6f7f3d76577bdcd7c3e7eb0a4e7ade6f24e
tsarni <tomi.sarni@cyberlightning.com>
2013-10-25 11:50:41 +0300

    latest developments

--- RealVirtualIntegration/CoapDevice/src/com/cyberlightning/android/coap/CoapDeviceSimulator.java
+++ RealVirtualIntegration/CoapDevice/src/com/cyberlightning/android/coap/CoapDeviceSimulator.java
@@ -239,17 +239,17 @@ public class CoapDeviceSimulator extends Activity implements Observer {
 	
 	private void showToast(String _message) {
 	    	
-	    	LayoutInflater inflater = getLayoutInflater();
-	    	View layout = inflater.inflate(R.layout.toast_layout,(ViewGroup) findViewById(R.id.toast_layout_root));
+	    LayoutInflater inflater = getLayoutInflater();
+	    View layout = inflater.inflate(R.layout.toast_layout,(ViewGroup) findViewById(R.id.toast_layout_root));
 
-	    	TextView text = (TextView) layout.findViewById(R.id.text);
-	    	text.setText(_message);
+	    TextView text = (TextView) layout.findViewById(R.id.text);
+	    text.setText(_message);
 
-	    	Toast toast = new Toast(getApplicationContext());
-	    	toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-	    	toast.setDuration(Toast.LENGTH_LONG);
-	    	toast.setView(layout);
-	    	toast.show();
+	    Toast toast = new Toast(getApplicationContext());
+	    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+	    toast.setDuration(Toast.LENGTH_LONG);
+	    toast.setView(layout);
+	    toast.show();
 	    	
 	}
 	    
@@ -456,6 +456,7 @@ public class CoapDeviceSimulator extends Activity implements Observer {
 	        			// New device
 	        			BleDeviceInfo deviceInfo = createDeviceInfo(device, rssi);
 	        			addDevice(deviceInfo);
+	        			receivedMessages.append("Message Received : " + device.getAddress()  + "\n");
 	        		} else {
 	        			// Already in list, update RSSI info
 	        			BleDeviceInfo deviceInfo = findDeviceInfo(device);
