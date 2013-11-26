5634c8bed16244feeb8b9f4ae168f830ecad325b
tsarni <tomi.sarni@cyberlightning.com>
2013-11-26 16:11:56 +0200

    Changed JUNIT version to 4.11

--- RealVirtualIntegration/RealVirtualInteraction/backend/src/test/java/com/cyberlightning/realvirtualinteraction/backend/HttpSocketWorkerTests.java
+++ RealVirtualIntegration/RealVirtualInteraction/backend/src/test/java/com/cyberlightning/realvirtualinteraction/backend/HttpSocketWorkerTests.java
@@ -7,15 +7,13 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class HttpSocketWorkerTests extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public HttpSocketWorkerTests( String testName )
     {
         super( testName );
     }
@@ -25,14 +23,17 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( HttpSocketWorkerTests.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
+        assertTrue( true );
+    }
+   
+    public void testApp2() {
         assertTrue( true );
     }
 }
---
+++ RealVirtualIntegration/RealVirtualInteraction/backend/src/test/java/com/cyberlightning/realvirtualinteraction/backend/WebSocketWorkerTests.java
@@ -0,0 +1,14 @@
+package com.cyberlightning.realvirtualinteraction.backend;
+
+import org.junit.Before;
+
+import junit.framework.TestCase;
+
+public class WebSocketWorkerTests extends TestCase {
+	
+	@Before
+	public void asd() {
+		
+	}
+	
+}
