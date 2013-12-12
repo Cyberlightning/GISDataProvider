a3704cfdc379c5c591537c1cc32c5a0df708316d
Sami Jylkka <sami.jylkka@cyberlightning.com>
2013-12-12 12:27:24 +0200

    'Select layer' -option added to the selection list

--- GIS/scripts/map.js
+++ GIS/scripts/map.js
@@ -3,6 +3,7 @@ var xmlDoc;
 
 (function() {
     var baseUrl = "http://localhost:9090/geoserver/";
+    // var baseUrl = "http://dev.cyberlightning.com:9091/geoserver/";
     var oldCoordinates = null;
 
     function parseServerCapabilities(response) {
@@ -10,6 +11,17 @@ var xmlDoc;
 
         xmlDoc = new DOMParser().parseFromString(response,'text/xml');
         var x = xmlDoc.getElementsByTagNameNS("http://www.opengis.net/w3ds/0.4.0", "Layer");
+
+        var combo = document.getElementById('selector');
+        var option = document.createElement('option');
+        option.text = "Select layer";
+        option.value = "select_layer";
+        try {
+            combo.add(option, null); //Standard 
+        } catch(error) {
+            combo.add(option); // IE only
+        }
+
 
         // console.log(x);
         for (i=0;i<x.length;i++)
@@ -23,7 +35,6 @@ var xmlDoc;
             } catch(error) {
                 combo.add(option); // IE only
             }
-            option.value = "";
         }
 
     }
@@ -53,11 +64,15 @@ var xmlDoc;
     // Traps selection list click event and launch layer detail fetching funtion
      $(function() {
         $("#selector").click(function(e) {
-            // console.log(this.options[this.selectedIndex].text);
+            // console.log("Selection list item: "+this.options[this.selectedIndex].value);
             e.preventDefault(); // if desired...
-
-          newLayer = true;
-          getLayerDetails(baseUrl, this.options[this.selectedIndex].text);
+            if (this.options[this.selectedIndex].value === 'select_layer'){
+                
+            }
+            else{
+                newLayer = true;
+                getLayerDetails(baseUrl, this.options[this.selectedIndex].text);
+            }
         });
       });
 
